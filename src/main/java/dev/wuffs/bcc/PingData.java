c9f622d2c3ed8d88bae36f2d951845291dac1566
Gareth Williams <gaz492@gmail.com>
2021-12-06 15:11:38 +0000

    Fix for possible null pointer crashes

--- src/main/java/dev/wuffs/bcc/PingData.java
+++ src/main/java/dev/wuffs/bcc/PingData.java
@@ -1,9 +1,9 @@
 package dev.wuffs.bcc;
 
 public class PingData {
-    public int projectID;
-    public String name;
-    public String version;
+    public int projectID = 0;
+    public String name = "INVALID/NOT_SET";
+    public String version = "INVALID/NOT_SET";
     public int versionID;
     public String releaseType;
     public boolean isMetadata = false;
