3d90c40c9577830ebc70f60315d5d33626248fd8
Gareth Williams <gaz492@gmail.com>
2021-12-06 15:12:31 +0000

    Fix for possible null pointer exception

--- src/main/java/dev/wuffs/bcc/PingData.java
+++ src/main/java/dev/wuffs/bcc/PingData.java
@@ -2,8 +2,8 @@ package dev.wuffs.bcc;
 
 public class PingData {
     public int projectID;
-    public String name;
-    public String version;
+    public String name = "INVALID/NOT_SET";
+    public String version = "INVALID/NOT_SET";
     public int versionID;
     public String releaseType;
     public boolean isMetadata = false;
