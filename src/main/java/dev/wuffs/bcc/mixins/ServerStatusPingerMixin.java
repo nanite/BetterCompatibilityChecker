fba77adf2e281442f17047489949f665cb1d2a8c
Gareth Williams <gaz492@gmail.com>
2023-06-25 13:58:06 +0100

    Credit

--- src/main/java/dev/wuffs/bcc/mixins/ServerStatusPingerMixin.java
+++ src/main/java/dev/wuffs/bcc/mixins/ServerStatusPingerMixin.java
@@ -3,11 +3,8 @@ package dev.wuffs.bcc.mixins;
 import dev.wuffs.bcc.contract.ServerDataExtension;
 import dev.wuffs.bcc.data.BetterStatus;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ServerStatusPinger;
 import net.minecraft.network.protocol.status.ClientboundStatusResponsePacket;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,8 +13,9 @@ import java.lang.reflect.Field;
 
 @Mixin(targets = "net/minecraft/client/multiplayer/ServerStatusPinger$1")
 public class ServerStatusPingerMixin {
-private static final Field SERVER_DATA_FIELD;
+    private static final Field SERVER_DATA_FIELD;
 
+    // Credit to https://github.com/Aizistral-Studios/No-Chat-Reports/blob/1.20-Unified/forge/src/main/java/com/aizistral/nochatreports/forge/mixins/client/MixinServerStatusPinger%241.java
     static {
         // We use reflection here because Mixin's AP dies when trying to process @Shadow of this lol
         try {
@@ -49,7 +47,7 @@ private static final Field SERVER_DATA_FIELD;
         BetterStatus betterData = ((ServerDataExtension) (Object) packet.status()).getBetterData();
         try {
             ((ServerDataExtension) SERVER_DATA_FIELD.get(this)).setBetterData(betterData);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
