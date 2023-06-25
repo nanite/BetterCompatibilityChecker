384cbfa237ea6044ac2e18a2e079a128829c7ef4
Gareth Williams <gaz492@gmail.com>
2023-06-25 13:56:40 +0100

    Fix crash

--- src/main/java/dev/wuffs/bcc/mixins/ServerStatusPingerMixin.java
+++ src/main/java/dev/wuffs/bcc/mixins/ServerStatusPingerMixin.java
@@ -2,7 +2,8 @@ package dev.wuffs.bcc.mixins;
 
 import dev.wuffs.bcc.contract.ServerDataExtension;
 import dev.wuffs.bcc.data.BetterStatus;
-import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerStatusPinger;
 import net.minecraft.network.protocol.status.ClientboundStatusResponsePacket;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,9 +12,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net/minecraft/client/multiplayer/ServerStatusPinger$1", remap = false)
+import java.lang.reflect.Field;
+
+@Mixin(targets = "net/minecraft/client/multiplayer/ServerStatusPinger$1")
 public class ServerStatusPingerMixin {
-    @Shadow @Final ServerData val$p_105460_;
+private static final Field SERVER_DATA_FIELD;
+
+    static {
+        // We use reflection here because Mixin's AP dies when trying to process @Shadow of this lol
+        try {
+            Class<?> pinger = Class.forName("net.minecraft.client.multiplayer.ServerStatusPinger$1");
+
+            SERVER_DATA_FIELD = pinger.getDeclaredField("val$p_105460_");
+            SERVER_DATA_FIELD.setAccessible(true);
+        } catch (Exception ex) {
+            Error error = new Error("Reflection failed in MixinServerStatusPinger$1!", ex);
+
+            Minecraft.getInstance().execute(() -> {
+                throw error;
+            });
+
+            throw error;
+        }
+    }
 
     @Inject(
             method = "handleStatusResponse(Lnet/minecraft/network/protocol/status/ClientboundStatusResponsePacket;)V",
@@ -26,6 +47,10 @@ public class ServerStatusPingerMixin {
     )
     void onHandleResponse(ClientboundStatusResponsePacket packet, CallbackInfo ci) {
         BetterStatus betterData = ((ServerDataExtension) (Object) packet.status()).getBetterData();
-        ((ServerDataExtension) val$p_105460_).setBetterData(betterData);
+        try {
+            ((ServerDataExtension) SERVER_DATA_FIELD.get(this)).setBetterData(betterData);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
